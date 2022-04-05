102565c7f7bc14aa99ac2d7db8e2107a173dfa65
Sedat Arslanargin <chemistercan@gmail.com>
2022-04-05 12:39:05 -0400

    I made some changes

--- src/SecondJavaClass.java
+++ src/SecondJavaClass.java
@@ -1,5 +1,7 @@
 public class SecondJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning Github");
+        System.out.println("Adding a new code");
+        System.out.println("this is another code");
     }
 }
